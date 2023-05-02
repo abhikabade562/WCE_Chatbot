530bee5d74eabe9df84ea180d03aa8fd2abf4bd0
Abhishek <abdkabade562@gmail.com>
2023-05-02 22:07:08 +0530

    first commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,14 @@
+def get_response():
+#     user_response = request.form['user_input']
+#     user_response = user_response.lower()
+#     if (user_response != 'bye'):
+#         if (user_response == 'thanks' or user_response == 'thank you'):
+#             return 'You are welcome...'
+#         else:
+#             if (greeting(user_response) != None):
+#                 return greeting(user_response)
+#             else:
+#                 return response(user_response)
+#                 sentence_tokens.remove(user_response)
+#     else:
+#         return 'bye!'
